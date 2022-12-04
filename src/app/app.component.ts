68a399018cdea3ee642086ec777343fd1c065df9
Debanshi_Saha <debanshi.tf@gmail.com>
2022-12-04 18:57:50 +0530

    debanshi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherData } from './models/weather.model';
+import { WeatherService } from './services/weather.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'WetherApp';
+export class AppComponent implements OnInit {
+
+  constructor(private WeatherSearvice: WeatherService){
+
+  }
+  
+  cityName: string ='Kolkata';
+  weatherData?: WeatherData;
+
+  defaultCityNAme =""
+
+  ngOnInit(): void {
+    this.getWeatherData(this.cityName);
+    this.defaultCityNAme = this.cityName;
+    this.cityName= "";
+
+  }
+  onSubmit(){
+    this.getWeatherData(this.cityName);
+    this.defaultCityNAme = this.cityName;
+    this.cityName= "";
+    
+  }
+
+  private getWeatherData(cityName:string){
+    this.WeatherSearvice.getWeatherData(cityName)
+    
+    .subscribe({
+      next: (response) => {
+        this.weatherData = response;
+        console.log(response);
+      }
+    });
+  }
+  
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,10 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
-};
+  production: false,
+  weatherApiBaseUrl: 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather',
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue: 'weather-by-api-ninjas.p.rapidapi.com',
+
+  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue: 'b4004ce93fmsh9e75267a6cf0488p1aa0bcjsne7965c5d5e74'
+};
